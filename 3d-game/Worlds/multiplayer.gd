a6a609e77c9c7d86ac533bfe614157c529676305
mathkimchi <kingalvinjkim@gmail.com>
2025-04-12 19:30:46 -0400

    spawn on both client and server (buggy)

--- 3d-game/Worlds/multiplayer.gd
+++ 3d-game/Worlds/multiplayer.gd
@@ -28,13 +28,16 @@ func host() -> void:
 	multiplayer.peer_connected.connect(self.add_player)
 	multiplayer.peer_disconnected.connect(self.remove_player)
 	multiplayer.server_disconnected.connect(self.server_delete)
+	
+	add_player(multiplayer.get_unique_id()) # should always be 1 for host
 
 func add_player(id):
 	print("Added: ", id)
 	var player_scene = preload("res://Entities/player.tscn")
 	var player = player_scene.instantiate()
+	player.name = "Player" + str(id)
 	# yeah, put this somewhere else
-	add_child(player)
+	$Players.add_child(player)
 
 func remove_player(id):
 	print("Removed: ", id)
